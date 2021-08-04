5c8e6d5b4c6c2007f82804eb0ac8c3cf72e310c7
Marriga <88306559+Marriga@users.noreply.github.com>
2021-08-04 13:13:32 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	float num = 3.5;
+	printf("%f\n", num);
+	char ch = 'A';
+	printf("%c\n", ch);
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(double));
+	return 0;
+
+}
